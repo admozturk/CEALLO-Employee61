c99aecbff9c1467e6ea93487113b06d625532f58
ADEM OZTURK <113047612+admozturk@users.noreply.github.com>
2023-03-18 23:10:58 +0100

    Update Hooks.java

--- src/test/java/com/ceallo/step_defs/Hooks.java
+++ src/test/java/com/ceallo/step_defs/Hooks.java
@@ -22,18 +22,18 @@ public class Hooks {
         login.button.click();*/
     }
 
- // @After
- //   public void teardownScenario(Scenario scenario){
+  @After
+   public void teardownScenario(Scenario scenario){
 
-        //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
+        scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
 
-      //  if (scenario.isFailed()){
-      //      byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-       //     scenario.attach(screenshot, "image/png", scenario.getName());
-     //   }
+      if (scenario.isFailed()){
+        byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+           scenario.attach(screenshot, "image/png", scenario.getName());
+       }
 
         //BrowserUtils.sleep(5);
-     //   Driver.closeDriver();
+      Driver.closeDriver();
 
         //System.out.println("====Closing browser using cucumber @After");
         //System.out.println("====Scenario ended/ Take screenshot if failed!");
